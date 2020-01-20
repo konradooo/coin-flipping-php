862023364218dc67906e760abe563ed9014ae0ca
konradooo <60118987+konradooo@users.noreply.github.com>
2020-01-21 00:58:33 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<title>coin flipping in php</title>
+<body bgcolor="lightblue">
+
+<!-- php script -->
+
+<?php
+	$flip = rand(0,1);
+	echo "<br>";
+	
+	if($flip === 0){
+		echo 'orzel';
+	}
+	else{
+		echo 'reszka';
+	}
+?>
+
+<br> <br>
+
+click <b>F5</b> and flip the coin again
+
+<br> <br>
+
+<b>made by konradooo</b>
+
+</body>
